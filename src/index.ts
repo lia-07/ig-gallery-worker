f360fd96ef6a72a9fac7e6cd3cfcd148dd30478f
lia-07 <107384739+lia-07@users.noreply.github.com>
2024-07-31 20:30:20 +1200

    make some major improvements

--- src/index.ts
+++ src/index.ts
@@ -1,48 +1,50 @@
 export interface Env {
-	IG_TOKENS: any;
+	IG_TOKENS: KVNamespace;
 }
 
 export default {
-	async fetch(request: { url: string | URL }, env: Env) {
+	async fetch(request: { url: string }, env: Env, ctx: ExecutionContext) {
 		const { searchParams } = new URL(request.url);
-		const profileName = searchParams.get('profile');
+		const key = await env.IG_TOKENS.get(searchParams.get('profile')!);
 		const fields = searchParams.get('fields');
 
-		const key = await env.IG_TOKENS.get(profileName);
+		if (!key)
+			return new Response('You must provide a valid profile name!', {
+				status: 400,
+			});
 
 		try {
-			const response = await fetch(`https://graph.instagram.com/me/media?fields=${fields}&access_token=${key}`);
-			return response;
-		} catch (error) {
-			return new Response(JSON.stringify({ error: error }), {
+			return await fetch(`https://graph.instagram.com/me/media?fields=${fields}&access_token=${key}`);
+		} catch (e) {
+			return new Response(`An error occurred: ${e}`, {
 				status: 500,
-				headers: { 'Content-Type': 'application/json' },
 			});
 		}
 	},
-	async scheduled(env: Env) {
-		console.log('Cron started');
+
+	async scheduled(event: ScheduledEvent, env: Env, ctx: ExecutionContext) {
+		console.info('Cron started');
+
 		const accounts = await env.IG_TOKENS.list();
 
-		loop: for (const account of accounts.keys) {
+		for (const account of accounts.keys) {
 			const token = await env.IG_TOKENS.get(account.name);
 
 			try {
 				const response = await fetch(`https://graph.instagram.com/refresh_access_token?grant_type=ig_refresh_token&access_token=${token}`);
-				if (response.ok) {
-					const data: any = await response.json();
-					const newToken = data.access_token;
-					if (token !== newToken) {
-						await env.IG_TOKENS.put(account.name, newToken);
-						console.log('Successfully updated token for ' + account.name);
-						continue loop;
-					}
-					console.log('Failed to update token for ' + account.name + ': too soon (token is still functional)');
-					continue loop;
+
+				if (!response.ok) {
+					console.error(`A ${response.status} error occurred when updating token for ${account.name}:  ${await response.text()} `);
+					continue;
 				}
-				console.error('A ' + response.status + ' error occurred, ' + (await response.text()));
-			} catch (error) {
-				console.error('An error occurred when fetching new token for ' + account.name + ': ' + error);
+
+				const data: { access_token: string } = await response.json();
+
+				await env.IG_TOKENS.put(account.name, data.access_token);
+
+				console.info('Successfully updated token for ' + account.name);
+			} catch (e) {
+				console.error('An error occurred when fetching new token for ' + account.name + ': ' + e);
 			}
 		}
 	},
